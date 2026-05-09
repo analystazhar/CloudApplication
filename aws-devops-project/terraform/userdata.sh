1af3a93f13fc7976c12c8847daa474c95d2cc85a
analyst azhar <azharuddinalam06@gmail.com>
2026-05-09 12:54:31 +0530

    updated the userdata

--- aws-devops-project/terraform/userdata.sh
+++ aws-devops-project/terraform/userdata.sh
@@ -5,5 +5,5 @@ sudo systemctl start httpd
 sudo systemctl enable httpd
 
 cat <<EOF > /var/www/html/index.html
-<h1>Terraform EC2 Server</h1>
+<h1>Terraform EC2 Server has been successfullt setup ans waiting for the production code from the dev team </h1>
 EOF
