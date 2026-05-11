4116cdc82e2f385b403e328d1bbabdb262336d80
Azharuddin Alam <126751174+analystazhar@users.noreply.github.com>
2026-05-11 10:02:39 +0000

    update the html code

--- aws-devops-project/terraform/userdata.sh
+++ aws-devops-project/terraform/userdata.sh
@@ -5,5 +5,10 @@ sudo systemctl start httpd
 sudo systemctl enable httpd
 
 cat <<EOF > /var/www/html/index.html
-<h1>Terraform EC2 Server has been successfullt setup ans waiting for the production code from the development team </h1>
+<h1><p>
+The infrastructure environment has been successfully provisioned and configured using Terraform on AWS. 
+The EC2 application server is operational and integrated with the required networking, security groups, 
+and load balancing components. The environment is now stable, scalable, and ready for deployment of the 
+production application code from the development team.
+</p></h1>
 EOF
