b750fa9780a965488b5e11a88e330dbc7e15b032
Azharuddin Alam <126751174+analystazhar@users.noreply.github.com>
2026-05-11 08:49:52 +0000

    Initial clean commit

---
+++ aws-devops-project/terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_1" {
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_2" {
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_1" {
+  default = "10.0.3.0/24"
+}
+variable "private_subnet_2" {
+  default = "10.0.4.0/24"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "your-keypair"
+}
+
+variable "db_username" {
+  default = "admin"
+}
+
+variable "db_password" {
+  default = "Password1234"
+}
